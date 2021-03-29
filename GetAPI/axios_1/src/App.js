8d60eacbee21dde863dbf81661eaf1f664e9cc4a
firatmelih <melih.firat@ogr.sakarya.edu.tr>
2021-03-29 17:01:18 +0300

    axios added, fetch updated

---
+++ GetAPI/axios_1/src/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import './styles/App.min.css'
+import axios from 'axios'
+import loading from './assets/img/loading.gif'
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [inputs, setInputs] = useState("");
+  useEffect(() => {
+    axios.get('https://restcountries.eu/rest/v2/all')
+      .then(res => setCountries(res.data))
+      .catch(err => console.log(err))
+  }, [])
+
+
+  return (
+    <div className="App">
+
+      <input
+        name="name"
+        placeholder="Search Country"
+        value={inputs.name}
+        onChange={e => setInputs(e.target.value)}
+      />
+      <div className="Countries">{countries.map(country => {
+        if (country.name === inputs) {
+          return (
+            <div key={Date.now}>
+              <h1>{country.name}</h1>
+              <img src={
+                country.flag ? country.flag : loading
+              }
+              ></img>
+              <h2>Capital: {country.capital}</h2>
+              <h2>Borders: {country.borders + ""} </h2>
+            </div>
+          )
+        }
+      }
+      )}</div>
+    </div >
+  );
+}
+
+export default App;
